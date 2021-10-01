5ec63a93c87841640199f9590e0adbe46b2a5dd4
moueza <>
2021-10-01 12:17:48 +0200

    sov

---
+++ jenkinsImageGenerator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/POUB/Human-Visible-Project-Blender-3D
+~/blender-2.93.4-linux-x64/blender -b  ~/POUB/Human-Visible-Project-Blender-3D/Human-Visible-Project-Blender-3D-manual-coarseBl293curves.blend -x 1  -F JPEG -f 0
+mv /tmp/0000.jpg ~/POUB/Human-Visible-Project-Blender-3D/Human-Visible-Project-Blender-3D-manual-coarseBl293curves.jpg
+cp  ~/POUB/Human-Visible-Project-Blender-3D/Human-Visible-Project-Blender-3D-manual-coarseBl293curves.jpg web
