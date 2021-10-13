346ed24162555f6f1148c2d90cf4cda8c8cfda55
moueza <>
2021-10-13 07:35:02 +0200

    ancient lib

--- Human-Visible-Project-Blender-3D-manual-coarseBl293curvesMERGER.py
+++ Human-Visible-Project-Blender-3D-manual-coarseBl293curvesMERGER.py
@@ -1,66 +1,32 @@
 # -*- coding: utf-8 -*
 #Bl2.93.3
 #10by10
-#https://stackoverflow.com/questions/10589620/syntaxerror-non-ascii-character-xa3-in-file-when-function-returns-%C2%A3
-#text edit http://jmsoler.free.fr/didacticiel/blender/tutor/python_script00_en.htm
 
-#py  https://docs.blender.org/api/current/info_quickstart.html
-#concat https://stackoverflow.com/questions/20441035/unsupported-operand-types-for-int-and-str#jkhk
-
-#https://stackoverflow.com/questions/11604548/running-python-script-in-blender
-#filename = "/home/peter/POUB/blender/DANSE/kizombaSemba/COURS/kizomba-Mervil-sem02-avance-Bl28.py"
-#exec(compile(open(filename).read(), filename, 'exec'))
-#
-
-# import bpy ; import os ; filename420547 = os.path.join(os.path.dirname(bpy.data.filepath), "kizomba-Mervil-sem02-avance-Bl28.py"); exec(compile(open(filename420547).read(), filename420547, 'exec'))
-
-#male armature color ? In pose mode, assign
-#IPO above 120 ? Photocopies output...end : 120->180
-
-
-#print(n,n+1 ,  " neutralisation")
-
-#quaternions : 
-
-
-#save
-#from https://github.com/moueza/covid19-corona-virus-scilab/blob/master/PYTHON/time_series_covid19_confirmed_global.py
-#from Magazine Linux magazine blue HS 40 H p84
+import bpy
+import os
+ 
 prenom = 88888
 text = "DDDDu in %s\n" % prenom
 #filename= "/home/peter/POUB/blender/DANSE/kizombaSemba/COURS/kizomba-Mervil-sem02-avance-Bl28.txt"
 filename435 = "/home/peter/POUB/blender/DANSE/kizombaSemba/COURS/poub.txt"
-#concat + and str()
-#with open('./'+kizomba-Mervil-sem02-avance-Bl28+'.txt') as csv_fileConfirmed:
-            #csv_reader = csv.reader(csv_fileConfirmed, delimiter=',')
-#https://stackoverflow.com/questions/31414263/python-using-open-w-filenotfounderror
-#l=str(line[0])+','+str(line[1])+','+str(line[2])+'\n'
+ 
 f = open(filename435,'a')
 print('lbl4')
 f.write(text)
 f.close
-            #line_count = 0
-            # print(f'longh = {line_count.length}')
-            # print(f'longh = {len(line_count)}') KO
-            # print(len(csv_reader)) KO
-            # https://docs.scipy.org/doc/numpy/reference/generated/numpy.matrix.html
-            #matt = np.matrix('1 2; 3 4')
-      
-            #arrayMoi = []
-            #for roww in csv_reader:
-                #if line_count != 0:
-                    #print(f'\t {line_count} {roww[1]} {int(roww[4]) + 1000}')
+    
 fileRef="Human-Visible-Project-Blender-3D-manual-coarseBl293curves.blend"
 print("lbl20 len = ",len(bpy.data.collections))
+
+
+
 #delete already ancient collections
 #generate for test
-
-
 for collection in bpy.data.collections :
    print(collection.name) 
    #bpy.data.collections.remove(collection)
    #or index       
-   if (not(-1 == collection.name.find('Coll', 0, 50))):
+   if (not(-1 == collection.name.find('Coll'))):
         print(True)
         #https://b3d.interplanety.org/en/removing-collections-through-the-blender-python-api/
         for obj in collection.objects:
@@ -74,3 +40,30 @@ for collection in bpy.data.collections :
         print(False)
    
 #print(bpy.data.collections[2])
+
+
+
+#import APPEND https://b3d.interplanety.org/en/how-to-append-an-object-from-another-blend-file-to-the-scene-using-the-blender-python-api/
+filepath = "/home/peter/POUB/Human-Visible-Project-Blender-3D/Human-Visible-Project-Blender-3D-manual-coarseBl293curves.blend"
+#file_path = 'D:/11.blend'bpy.ops.wm.append( filename="Human-Visible-Project-Blender-3D-manual-coarseBl293curves.blend")
+#inner_path = 'Collection'
+#object_name = 'Suzanne'
+#bpy.ops.wm.append(
+#    filepath=os.path.join(file_path, inner_path, object_name),
+#    directory=os.path.join(file_path, inner_path),
+#    filename=object_name
+#    )
+
+#blender python api objects of another file
+#https://blender.stackexchange.com/questions/75746/import-multiple-objects-using-python
+#    ancien lib doc   https://docs.blender.org/api/blender_python_api_current/bpy.types.BlendDataLibraries.html?highlight=blenddatalibraries
+#    modern lib doc https://docs.blender.org/api/current/bpy.types.BlendDataLibraries.html
+with bpy.data.libraries.load(filepath) as (data_from, data_to):
+    #data_to.objects = [name for name in data_from.objects if name.startswith("house")]
+    data_to.objects = [name for name in data_from.objects ]
+
+# link them to scene
+scene = bpy.context.scene
+for obj in data_to.objects:
+    if obj is not None:
+        scene.objects.link(obj)
